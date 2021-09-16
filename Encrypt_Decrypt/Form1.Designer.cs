2e11431415724b89abf7b878dd59c5755b28a72d
EsiLambda <ehsan.qaffari@gmail.com>
2021-09-16 12:49:10 +0430

    Encrypt-Decrypt needed files, folders and classes

---
+++ Encrypt_Decrypt/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Encrypt_Decrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblJump = new System.Windows.Forms.Label();
+            this.txtJump = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(84, 31);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(130, 22);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(259, 28);
+            this.btnEncrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(100, 28);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(81, 120);
+            this.lblOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 16);
+            this.lblOutput.TabIndex = 7;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(259, 70);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(100, 28);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblInput.Location = new System.Drawing.Point(16, 31);
+            this.lblInput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(39, 16);
+            this.lblInput.TabIndex = 4;
+            this.lblInput.Text = "Input:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(16, 120);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Result:";
+            // 
+            // lblJump
+            // 
+            this.lblJump.AutoSize = true;
+            this.lblJump.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblJump.Location = new System.Drawing.Point(16, 76);
+            this.lblJump.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblJump.Name = "lblJump";
+            this.lblJump.Size = new System.Drawing.Size(44, 16);
+            this.lblJump.TabIndex = 5;
+            this.lblJump.Text = "Jump:";
+            // 
+            // txtJump
+            // 
+            this.txtJump.Location = new System.Drawing.Point(84, 73);
+            this.txtJump.Margin = new System.Windows.Forms.Padding(4);
+            this.txtJump.Name = "txtJump";
+            this.txtJump.Size = new System.Drawing.Size(130, 22);
+            this.txtJump.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 151);
+            this.Controls.Add(this.lblJump);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtJump);
+            this.Controls.Add(this.txtInput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblJump;
+        private System.Windows.Forms.TextBox txtJump;
+    }
+}
+
---
+++ Encrypt_Decrypt/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encrypt_Decrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            int[] asciiArr = new int[txtInput.Text.Length];
+            int jump = 1;
+
+            if((int)txtJump.Text[0] - 48 <= -1 || (int)txtJump.Text[0] - 48 >= 10)
+            {
+                MessageBox.Show("Invalid number for jump!");
+            }
+            else
+            {
+                jump = (int)txtJump.Text[0] - 48;
+            }
+
+            lblOutput.Text = "";
+
+            for (int i = 0; i <= txtInput.Text.Length - 1; i++)
+            {
+                if ((int)txtInput.Text[i] + jump >= 58 && ((int)txtInput.Text[i] >= 48 && (int)txtInput.Text[i] <= 57))
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] + jump - 10;
+                }
+                else if ((int)txtInput.Text[i] + jump >= 91 && ((int)txtInput.Text[i] >= 65 && (int)txtInput.Text[i] <= 90))
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] + jump - 26;
+                }
+                else if((int)txtInput.Text[i] + jump >= 123 && ((int)txtInput.Text[i] >= 97 && (int)txtInput.Text[i] <= 122))
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] + jump - 26;
+                }
+                else if ((int)txtInput.Text[i] == 32)
+                {
+                    asciiArr[i] = (int)txtInput.Text[i];
+                }
+                else
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] + jump;
+                }
+            }
+
+            foreach (int asciiCode in asciiArr)
+            {
+                lblOutput.Text += ((char)asciiCode).ToString();
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            int[] asciiArr = new int[txtInput.Text.Length];
+            int jump = 1;
+
+            if ((int)txtJump.Text[0] - 48 <= -1 || (int)txtJump.Text[0] - 48 >= 10)
+            {
+                MessageBox.Show("Invalid number for jump!");
+            }
+            else
+            {
+                jump = (int)txtJump.Text[0] - 48;
+            }
+
+            lblOutput.Text = "";
+
+            for (int i = 0; i <= txtInput.Text.Length - 1; i++)
+            {
+                if ((int)txtInput.Text[i] - jump <= 47 && ((int)txtInput.Text[i] >= 48 && (int)txtInput.Text[i] <= 57))
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] - jump + 10;
+                }
+                else if ((int)txtInput.Text[i] - jump <= 64 && ((int)txtInput.Text[i] >= 65 && (int)txtInput.Text[i] <= 90))
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] - jump + 26;
+                }
+                else if ((int)txtInput.Text[i] - jump <= 96 && ((int)txtInput.Text[i] >= 97 && (int)txtInput.Text[i] <= 122))
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] - jump + 26;
+                }
+                else if((int)txtInput.Text[i] == 32)
+                {
+                    asciiArr[i] = (int)txtInput.Text[i];
+                }
+                else
+                {
+                    asciiArr[i] = (int)txtInput.Text[i] - jump;
+                }
+            }
+
+            foreach (int asciiCode in asciiArr)
+            {
+                lblOutput.Text += ((char)asciiCode).ToString();
+            }
+        }
+    }
+}
